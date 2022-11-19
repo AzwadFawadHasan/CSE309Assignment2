6212301505d290cd1ed71c3eaca0d40035c08506
azwad <azwadfawadhasan@gmail.com>
2022-11-19 23:40:19 +0600

    created save.php

---
+++ save.php
@@ -0,0 +1,8 @@
+<?php
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $subject=$_POST['subject'];
+    $contactNumber=$_POST['contactNumber'];
+    $issue=$_POST['issue'];
+    echo "$name , $email";
+?>
